1942a07cdea8ebc6536f630b35b3768f217f4e4c
denisschmidt <parasolgroup25@gmail.com>
2023-10-18 15:22:26 +0300

    feat: daily edu syscall

--- contracts/Multi Call/main.sol
+++ contracts/Multi Call/main.sol
@@ -17,9 +17,23 @@ contract TestMultiCall {
 }
 
 Staticcall
-The syntax to query contracts using staticcall is similar to using call
+    The syntax to query contracts using staticcall is similar to using call
 
-(bool success, bytes memory response) = contractToCall.staticcall(data)
+    (bool success, bytes memory response) = contractToCall.staticcall(data)
+
+
+Task:
+    - Complete the function
+
+        function multiCall(address[] calldata targets, bytes[] calldata data)
+
+        This function should
+
+        For each address in targets use staticcall to call targets[i] passing data[i].
+        Fail if any call to address in targets fails
+        Save the result in results bytes array.
+        Return all the results stored in results.
+        Fail if targets.length differs from data.length.
 
 */
 
@@ -31,7 +45,17 @@ contract MultiCall {
         address[] calldata targets,
         bytes[] calldata data
     ) external view returns (bytes[] memory) {
+        require(targets.length == data.length, "target length != data length");
+
         bytes[] memory results = new bytes[](data.length);
+
+        for (uint i = 0; i < targets.length; i++) {
+            (bool success, bytes memory response) = targets[i].staticcall(data[i]);
+            require(success, "call failed");
+            results[i] = response;
+        }
+
+        require(results.length == data.length, "res != data");
 
         return results;
     }
